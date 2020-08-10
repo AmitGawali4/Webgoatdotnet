a46e695bfa16c5c431051897c36a471f626a67e5
AmitGawali4 <39050953+AmitGawali4@users.noreply.github.com>
2020-08-10 17:01:26 +0530

    Update Startup.cs

--- WebGoat/Startup.cs
+++ WebGoat/Startup.cs
@@ -1,6 +1,1 @@
-using Microsoft.IdentityModel.Protocols.OpenIdConnect;
-using Microsoft.IdentityModel.Tokens;
-using Microsoft.Owin.Security;
-using Microsoft.Owin.Security.Cookies;
-using Microsoft.Owin.Security.Notifications;
-using Microsoft.Owin.Security.OpenIdConnect;
+
